cfcfea5c88eb06f3efcbd3c7af8ed26819fef6dd
wally-pacmero <74281685+wally-pacmero@users.noreply.github.com>
2022-07-04 16:38:59 -0500

    Update SubClaim.php

--- src/subclaim/SubClaim.php
+++ src/subclaim/SubClaim.php
@@ -3,9 +3,22 @@
 namespace subclaim;
 
 use pocketmine\plugin\PluginBase;
+use subclaim\SubclaimListener;
 
 class Subclaim extends PluginBase {
 
+use SingletonTrait;
 
+    protected function onLoad(): void {
+        self::setInstance($this);
+    }
+
+    protected function onEnable(): void {
+$this->registerListener(new SubclaimListener());
+}
+
+    private function registerListener(Listener $listener): void {
+        $this->getServer()->getPluginManager()->registerEvents($listener, $this);
+    }
 
 }
