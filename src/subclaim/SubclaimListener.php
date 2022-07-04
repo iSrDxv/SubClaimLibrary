32f05ad2a5c22f2a055bf8c02d1e599f0b588c9c
wally-pacmero <74281685+wally-pacmero@users.noreply.github.com>
2022-07-04 16:42:19 -0500

    Create SubclaimListener.php

---
+++ src/subclaim/SubclaimListener.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace subclaim;
+
+use pocketmine\event\Listener;
+use pocketmine\player\Player;
+pocketmine\event\player\PlayerInteractEvent;
+
+class SubclaimListener implements Listener {
+
+public function onInteract(PlayerInteractEvent $event): void {
+// code...
+}
+
+}
